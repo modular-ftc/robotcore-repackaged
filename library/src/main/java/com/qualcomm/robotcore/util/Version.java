db8b440eaefe1b984c8d1fb0c74423c965b0b1a2
Mitchell Skaggs <skaggsm333@gmail.com>
2018-10-01 17:29:19 -0500

    Add two decompiled files not found in the sources jar

---
+++ library/src/main/java/com/qualcomm/robotcore/util/Version.java
@@ -0,0 +1,13 @@
+/*
+ * Decompiled with CFR 0_133.
+ */
+package com.qualcomm.robotcore.util;
+
+public class Version {
+    public static final String LIBRARY_VERSION = "developer_build";
+
+    public static String getLibraryVersion() {
+        return LIBRARY_VERSION;
+    }
+}
+
